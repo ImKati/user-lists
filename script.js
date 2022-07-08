bb291acede2d450745995614bf9cec81b34f36b8
ImKati <kati.mihalevich@gmail.com>
2022-07-08 14:43:48 +0300

    two commit

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
 window.onload = function () {
+    let users_table_div = document.getElementById('users-table-div');
+    users_table_div.innerHTML += "<table id = 'users-table''>";
     fetch('https://jsonplaceholder.typicode.com/users')
-        .then(response => response.json())
-        .then(data => data.forEach((album) => {
-            document.body.insertAdjacentHTML('beforeEnd', `
-<p>${album.userId}</p>
-<p>${album.id}</p>
-<p>${album.title}</p>
-`)
-        }))
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            let users_table = document.getElementById('users-table');
+            data.forEach(val => {
+                let tr = "<tr>" +
+                    "<td>" + val['id'] + "</td>" +
+                    "<td>" + val['name'] + "</td>" +
+                    "<td>" + val['username'] + "</td>" +
+                    "<td>" + val['email'] + "</td>" +
+                    "<td>" + val['address']['street'] + "</td>" +
+                    "<td>" + val['address']['city'] + "</td>" +
+                    "<td>" + val['phone'] + "</td>" +
+                    "<td>" + val['website'] + "</td>" +
+                    "</tr>"
+                users_table.innerHTML += tr
+            });
+        })
+        .then(users_table_div.innerHTML += "</table>");
 };
