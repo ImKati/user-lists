978cb7af670607211b7a78bb672ac48ddfeeb91e
ImKati <kati.mihalevich@gmail.com>
2022-07-13 13:31:18 +0300

    js2.0

--- script.js
+++ script.js
@@ -7,6 +7,30 @@ window.onload = function () {
         })
         .then((data) => {
             let users_table = document.getElementById('users-table');
+            let heading1 = document.createElement('th');
+            heading1.innerHTML = "Id";
+            let heading2 = document.createElement('th');
+            heading2.innerHTML = "Name";
+            let heading3 = document.createElement('th');
+            heading3.innerHTML = "Username";
+            let heading4 = document.createElement('th');
+            heading4.innerHTML = "Email";
+            let heading5 = document.createElement('th');
+            heading5.innerHTML = "Street";
+            let heading6 = document.createElement('th');
+            heading6.innerHTML = "City";
+            let heading7 = document.createElement('th');
+            heading7.innerHTML = "Phone";
+            let heading8 = document.createElement('th');
+            heading8.innerHTML = "Website";
+            users_table.appendChild(heading1);
+            users_table.appendChild(heading2);
+            users_table.appendChild(heading3);
+            users_table.appendChild(heading4);
+            users_table.appendChild(heading5);
+            users_table.appendChild(heading6);
+            users_table.appendChild(heading7);
+            users_table.appendChild(heading8);
             data.forEach(user => {
                 let tr = "<tr>" +
                     "<td>" + user['id'] + "</td>" +
